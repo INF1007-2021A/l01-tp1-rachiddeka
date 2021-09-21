b5296382b7cfcaa8ac428d152ef1d74496d1e0ac
rachiddeka <chaidadeka@gmail.com>
2021-09-21 12:01:45 -0400

    ok

--- exercice1.py
+++ exercice1.py
@@ -1,12 +1,12 @@
 def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
-    if n % 3 == 0 :
-        resultat = "fizz"
+    if n % 3 == 0 and n % 5 == 0 :
+        resultat = "fizzbuzz"
     elif n % 5 == 0:
         resultat = "buzz"
-    elif n % 3 == 0 and n % 5 == 0 :
-        resultat = "fizzbuzz"
+    elif n % 3 :
+        resultat = "fizz"
     else:
         resultat = n
     return resultat
--- exercice3.py
+++ exercice3.py
@@ -2,22 +2,26 @@
 def decomposer(secondes):
 
     # TODO: Assigner à la variable "annees" le nombre d'années
-    annees =
+    annees = int(secondes / 31540000)
+    secondes = secondes - (annees * 31540000)
 
     # TODO: Assigner à la variable "semaines" le nombre de semaines restantes
-    semaines =
+    semaines = int(secondes / 604800)
+    secondes = secondes - (semaines * 604800)
 
     # TODO: Assigner à la variable "jours" le nombre de jours restants
-    jours =
+    jours = int(secondes / 86400)
+    secondes = secondes - (jours * 86400)
 
     # TODO: Assigner à la variable "heures" le nombre d'heures restantes
-    heures =
+    heures = int(secondes / 3600)
+    secondes = secondes - (heures * 3600)
 
     # TODO: Assigner à la variable "minute" le nombre de minutes restantes
-    minutes =
+    minutes = int(secondes / 60)
 
     # TODO: Assigner à la variable "secondes" le nombre de secondes restantes
-    secondes =
+    secondes = secondes - (minutes * 60)
 
     # TODO: Afficher le nombres d'années, semaines, jours, heures, minutes et secondes
     print(annees ,semaines ,jours ,heures ,minutes ,secondes)
