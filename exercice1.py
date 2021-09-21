b4f66b7b63b8f4aba7bc348ffab88613ff6aa51c
rachiddeka <chaidadeka@gmail.com>
2021-09-21 11:13:32 -0400

    ok

--- exercice1.py
+++ exercice1.py
@@ -1,8 +1,14 @@
 def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
-
-    resultat =
+    if n % 3 == 0 :
+        resultat = "fizz"
+    elif n % 5 == 0:
+        resultat = "buzz"
+    elif n % 3 == 0 and n % 5 == 0 :
+        resultat = "fizzbuzz"
+    else:
+        resultat = n
     return resultat
 
 if __name__ == '__main__':
